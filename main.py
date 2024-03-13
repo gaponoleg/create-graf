e7ad67f6a4ba51697701ec4d689ade8031053a3c
gaponoleg <70619196+gaponoleg@users.noreply.github.com>
2024-03-13 21:36:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
-
+#новый словарь для замены значений
 mapping = {'robot': 0, 'human': 1}
-
+#использование oneHot
 data['one_hot'] = data['whoAmI'].map(mapping)
 
 print(data.head())
